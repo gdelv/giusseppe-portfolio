fae05b1c6bcf2551ac844e32dd1e240fb917862e
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2023-01-20 11:00:32 -0500

    added headshot and email link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
+import React from "react";
 import { useInView } from "react-intersection-observer";
 // import ShowContainer from './shared/ShowContainer'
-import THRESHOLD from '../data/constants.js'
-
+import THRESHOLD from "../data/constants.js";
 
 function Contact() {
-    // const [inView, setInView] = useState(false)
-    const { ref, inView, entry } = useInView({
-        /* Optional options */
-        threshold: THRESHOLD,
-      });
-      console.log(entry, "this be the entry");
+  // const [inView, setInView] = useState(false)
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: THRESHOLD,
+  });
+  console.log(entry, "this be the entry");
 
-    return (
-        <div id='contact' ref={ref}>
-            <h2 className={inView ? "show delay-0" : "hidden hide-left"}>Get In Touch</h2>
-            {/* <h2 className={inView ? "show delay-0" : "hidden hide-right"}>Touch</h2> */}
-            <h2 className={inView ? "show delay-3" : "hidden hide-left"}>giudelvecchio@gmail.com</h2>
-            {/* Social Media Links */}
-        </div>
-    )
+  return (
+    <div id="contact" ref={ref}>
+      <h2 className={inView ? "show delay-0" : "hidden hide-left"}>
+        Get In Touch
+      </h2>
+      {/* <h2 className={inView ? "show delay-0" : "hidden hide-right"}>Touch</h2> */}
+      <h2 className={inView ? "show delay-3" : "hidden hide-left"}>
+        <a
+          href="mailto:giudelvecchio@gmail.com?subject=Let's Connect!&body=Shoot me an email and I will respond ASAP."
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          giudelvecchio@gmail.com
+        </a>
+      </h2>
+      {/* Social Media Links */}
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useInView } from "react-intersection-observer";
 import THRESHOLD from '../data/constants.js'
+import HEADSHOT from '../data/images/Gheadshot.jpeg'
 
 export default function Intro() {
   const { ref, inView, entry } = useInView({
@@ -19,15 +20,15 @@ export default function Intro() {
       >{`Software Engineer`}</h2>
       <img
         width="200"
-        height="300"
+        height="200"
         className={inView ? "show delay-0" : "hidden hide-right "}
-        src="https://images.pexels.com/photos/3671194/pexels-photo-3671194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        src={HEADSHOT}
         alt="dinosaur"
         srcset=""
       />
-      <h2
+      {/* <h2
         className={inView ? "show delay-4" : "hidden hide-right"}
-      >{`(Not an actual picture of me)`}</h2>
+      >{`(Not an actual picture of me)`}</h2> */}
     </div>
   );
 }
