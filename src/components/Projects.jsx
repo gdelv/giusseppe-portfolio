4c7e60c1036fc18bde0823d14a4efc8d9abf9493
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2023-01-21 06:13:24 -0500

    website changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,69 +1,90 @@
-import React, { useState } from 'react'
-import { projects } from '../data';
+import React, { useState } from "react";
+import { projects } from "../data";
 import { useInView } from "react-intersection-observer";
-import THRESHOLD from '../data/constants.js'
+import THRESHOLD from "../data/constants.js";
 // import ShowContainer from './shared/ShowContainer';
 
 function Projects() {
-    const [showDetail, setShowDetail] = useState(false)
-    const [currentProj, setCurrentProj] = useState({})
-    const [fadeIn, setFadeIn] = useState(false)
-    const { ref, inView, entry } = useInView({
-        /* Optional options */
-        threshold: THRESHOLD,
-    });
-    console.log(entry, "this be the entry");
+  const [showDetail, setShowDetail] = useState(false);
+  const [currentProj, setCurrentProj] = useState({});
+  const [fadeIn, setFadeIn] = useState(false);
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: THRESHOLD,
+  });
+  console.log(entry, "this be the entry");
 
-    if (!fadeIn) {
-        setFadeIn(true)
-    }
-    const openProject = (e) => {
-        setFadeIn(false)
-        console.log(e.target.name);
-        let foundProj = projects.find(proj => proj.name === e.target.name)
-        setCurrentProj(foundProj)
-        setTimeout(() => {
-            setShowDetail(true)
-        }, 500);
-        //reverse projects entrance
-        //fade in information for project
-    }
-    const closeProject = () => {
-        console.log('clicked')
-        // console.log(e.taget.name)
-        setCurrentProj({})
-        setShowDetail(false)
-    }
-    return (
-        showDetail ?
-            //if inView and showDetail 
-            <div className='detail-container' ref={ref}>
-                <div className={inView ? 'detail-title egg' : "hidden hide-right"}>
-                    <h2>{currentProj.name}</h2>
-                    {/* <p className={inView ? "show" : "hidden hide-right"}>test</p> */}
-                    <button onClick={closeProject} className='detail-btn'>Return</button>
-                </div>
-            </div> :
-            //if inView and !showDetail
-            <div id='projects' ref={ref}>
-                <h2 className={inView ? "show" : "hidden hide-right"}>Projects</h2>
-                {/* <h2 className={inView && currentProj.length === 0 ? "show" : "hidden hide-right"}>Projects</h2> */}
-                <div className='projects-container'>
-                    {projects.map((project, index) => {
-                        let hideOption = index % 2 === 0 ? "hide-left" : "hide-right"
-                        return (
-                            <div className={inView ? `show project-card delay-${index + 1}` : `hidden ${hideOption}`}>
-                                <p className='project-title'>
-                                    {project.name}
-                                    <button name={project.name} onClick={openProject} className='project-btn'>See More</button>
-                                </p>
-                            </div>
-                        )
-                    })}
-                </div>
+  if (!fadeIn) {
+    setFadeIn(true);
+  }
+  const openProject = (e) => {
+    setFadeIn(false);
+    console.log(e.target.name);
+    let foundProj = projects.find((proj) => proj.name === e.target.name);
+    setCurrentProj(foundProj);
+    setTimeout(() => {
+      setShowDetail(true);
+    }, 100);
+    //reverse projects entrance
+    //fade in information for project
+  };
+  const closeProject = () => {
+    console.log("clicked");
+    // console.log(e.taget.name)
+    setCurrentProj({});
+    setShowDetail(false);
+  };
+  return showDetail ? (
+    //if inView and showDetail
+    <div className="detail-container" ref={ref}>
+      <div className={inView ? "detail-title" : "hidden hide-right"}>
+        <h2>{currentProj.name}</h2>
+        <img
+          src={currentProj.img}
+          alt={currentProj.name}
+          className="project-img"
+        />
+        <p className="detail-desc">{currentProj.description}</p>
+        <button onClick={closeProject} className="detail-btn">
+          Return
+        </button>
+        <button className="detail-btn">
+            <a href={currentProj.url} target="_blank" rel="noopener noreferrer">See It Live</a>
+        </button>
+      </div>
+    </div>
+  ) : (
+    //if inView and !showDetail
+    <div id="projects" ref={ref}>
+      <h2 className={inView ? "show" : "hidden hide-right"}>Projects</h2>
+      {/* <h2 className={inView && currentProj.length === 0 ? "show" : "hidden hide-right"}>Projects</h2> */}
+      <div className="projects-container">
+        {projects.map((project, index) => {
+          let hideOption = index % 2 === 0 ? "hide-left" : "hide-right";
+          return (
+            <div
+              className={
+                inView
+                  ? `show project-card delay-${index + 1}`
+                  : `hidden ${hideOption}`
+              }
+            >
+              <p className="project-title">
+                {project.name}
+                <button
+                  name={project.name}
+                  onClick={openProject}
+                  className="project-btn"
+                >
+                  See More
+                </button>
+              </p>
             </div>
-
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
--- src/data/index.js
+++ src/data/index.js
@@ -1,18 +1,31 @@
+const lorenzoWebsite = require("../data/images/lorenzoWebsite.gif");
+const jackWebsite = require("../data/images/jackwebsite.gif");
+const franoWebsite = require("../data/images/franoWebsite.gif");
+const survivorWebsite = require("../data/images/SurvivorPool.gif");
 export const projects = [
-    {
-        name: "Jack Portfolio",
-        url: "www.google.com"
-    },
-    {
-        name: "Frano Portfolio",
-        url: "www.google.com"
-    },
-    {
-        name: "Lorenzo Portfolio",
-        url: "www.google.com"
-    },
-    {
-        name: "NFL Survivor",
-        url: "www.google.com"
-    }
-]
+  {
+    name: "Jack Portfolio",
+    url: "https://www.jacksimonmakes.com/",
+    img: jackWebsite,
+    description:
+      "The website is a platform built using React for the front-end and Firebase for the back-end, it displays all the videos of director Jack Simon. The website allows the director to have control over the website's content by adding more information to the database and selecting which videos are displayed on the homepage.",
+  },
+  {
+    name: "Frano Portfolio",
+    url: "https://frano.netlify.app/",
+    img: franoWebsite,
+    description: "The website is a platform developed using React, it is designed for Phone Fix It repair stores. The website provides contact information and details about the services offered by the store. The website has a modern design and user interface. It aim to provide a seamless experience for the user to find the information they need about the store's services and location.",
+  },
+  {
+    name: "Lorenzo Portfolio",
+    url: "https://1stcallhvacsolutions.com",
+    img: lorenzoWebsite,
+    description: "The website is a platform built using React for the front-end, it is designed for the HVAC business 1stCallHVACSolutions. The website provides contact information and details about the services offered by the company. Additionally, the website features a web-to-email form that allows users to request service or ask for further information. The website has a modern design and user interface, it aims to provide a seamless and easy experience for the users to find information about the company's services and get in touch with the business.",
+  },
+  {
+    name: "NFL Survivor",
+    url: "https://survival-pool.netlify.app",
+    img: survivorWebsite,
+    description: "The website is a platform built using React for the front-end and Firebase for the back-end, it is focused on the NFL schedule and team records. The website displays the NFL weekly schedule color-coded by the current team records. Users can also post their weekly picks and at the end of the week, a batch process is used to determine which users made the correct picks. The website aims to provide an easy way for users to stay informed about the NFL schedule and make predictions about the outcome of the games.",
+  },
+];
